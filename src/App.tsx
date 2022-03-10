474faa5cd7a39ff39d40856d83e43d56ac9962ec
Tigran <tik.sargsyan105@gmail.com>
2022-03-10 10:43:37 +0400

    warning was solved

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React, {useEffect, useState} from "react";
 import Card, {CardVariant} from "./components/Card";
-import UserList from "./components/UserList";
 import {ITodo, IUser} from "./types/types";
 import axios from "axios";
 import List from "./components/List";
