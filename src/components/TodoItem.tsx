4816df5813fdb1d3cb3a640131dd9cdffe7f8e29
Tigran <tik.sargsyan105@gmail.com>
2022-03-10 10:41:09 +0400

    change style todo

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -9,8 +9,15 @@ const TodoItem: FC<TodoItemProps> = ({todo}) => {
     const [checked, setChecked] = useState(todo.completed)
     return (
         <div style={{padding: "15px", border: "1px solid black"}}>
-            <input id={todo.id + ""} type="checkbox" checked={checked} onChange={() => setChecked(!checked)}/>
-            <label htmlFor={todo.id + ""}>
+            <input id={todo.id + ""}
+                   type="checkbox"
+                   checked={checked}
+                   onChange={() => setChecked(!checked)}
+                   style={{cursor: "pointer"}}
+            />
+            <label htmlFor={todo.id + ""}
+                   style={{cursor: "pointer"}}
+            >
                 {todo.id}, {`${todo.title}`}
             </label>
         </div>
